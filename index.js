b28cfeca1002956f2044d54c903523c2fb8ca9a8
Badineedi.Krishna <Badineedi.Krishna@ivycomptech.com>
2023-05-10 19:58:16 +0530

    2nd Commit:setting up view engine and view Folder

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express =require('express');
-const app=express();
+const express = require('express');
+const app = express();
+const port = 8001;
 
-const port=8001;
+// set view engine and views folder
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.listen(port,(err)=>{
-    if(err){
-        console.log(`Error in starting the server at port: ${port} `+err);
+
+
+app.get('/', (req, res) => {
+    return res.render('home');
+});
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(`Error in starting the server at port: ${port} ` + err);
     }
     console.log(`Server is runnig at port: ${port}`);
 });
